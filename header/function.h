8ab5fe77c32edb123f9b61646a50c212e72a756b
Vincent Cadoret <vincent.cadoret@cpe.fr>
2021-10-22 08:35:20 +0200

    v2.2 function.h

--- header/function.h
+++ header/function.h
@@ -1,20 +1,31 @@
-//
-// Created by v.cadoret on 20/10/2021.
-//
-
 #ifndef PROGRAMMATION_DYNAMIQUE_FUNCTION_H
 #define PROGRAMMATION_DYNAMIQUE_FUNCTION_H
 
-#define SACK_MAX_SIZE 100
-
+/**
+ * @author Vincent Cadoret
+ * @brief Generation d'une structure pour les Sack.
+ * @param value
+ * @param weight
+ */
 typedef struct {
     float value;
     float weight;
 } Sack;
 
-#endif //PROGRAMMATION_DYNAMIQUE_FUNCTION_H
+/**
+ * @author Vincent Cadoret
+ * @brief Génére une Structure pour le PGCB.
+ * @param x
+ * @param y
+ */
+typedef struct {
+    float x;
+    float y;
+} Square;
 
 int *getArray(int size);
 int find_by_dichotomy(int array[], int size_t, int value);
 void triSack(Sack array[], int size);
 float find_by_knapsack(Sack array[], int size, float weight);
+
+#endif //PROGRAMMATION_DYNAMIQUE_FUNCTION_H
