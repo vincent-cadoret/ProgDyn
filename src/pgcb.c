9616fcc112f874ca11e11585338a7ae2724bda5e
Vincent Cadoret <vincent.cadoret@cpe.fr>
2021-10-22 17:04:50 +0200

    v3.2 exo3

--- src/pgcb.c
+++ src/pgcb.c
@@ -4,6 +4,13 @@
 #include <time.h>
 #include "../header/pgcb.h"
 
+/**
+ * @author Vincent Cadoret
+ * @brief Crée un tableau 1D pour la matrice.
+ * @param row
+ * @param column
+ * @return int *
+ */
 int *getMatrix(const int row, const int column){
     int size = row * column;
     int *matrix = malloc(size * sizeof(int));
@@ -11,14 +18,29 @@ int *getMatrix(const int row, const int column){
 
     for(int i = 0 ; i <  size - 1; i++) {
         matrix[i] = rand() % 2;
-        // printf("%d", matrix[i]);
     }
     return matrix;
 }
+
+/**
+ * @author Vincent Cadoret
+ * @brief Affiche la matrice dans le bon format.
+ * @param matrix
+ * @param row
+ * @param column
+ */
 void seeMatrix(int *matrix, const int row, const int column){
+    int k = 0;
     for(int i = 0 ; i < row ; i++) {
         for(int j = 0 ; j < column ; j++){
-            printf("%d  ", matrix[i*j]);
+            // printf("%d  ", matrix[k]);
+            if(matrix[k] == 0){
+                printf("   ");
+            }
+            if(matrix[k] == 1){
+                printf(" * ");
+            }
+            k++;
         }
         printf("\n");
     }
